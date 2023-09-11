4432de084719b93b7e38ee6f5e030b1ade2dc2c4
gagewillette <gagew0513@gmail.com>
2023-09-11 00:36:41 -0400

    created terrain and started with trees

--- Assets/Scripts/Test/PokerGameLogic.cs
+++ Assets/Scripts/Test/PokerGameLogic.cs
@@ -9,13 +9,17 @@ public class PokerGameLogic : MonoBehaviour
     private Dictionary<GameObject, Card[]> playerCards = new Dictionary<GameObject, Card[]>();
     private Card[] publicCards;
 
+    private GameObject winner;
+
     public static Action onNewPubilcCard;
-    
+
     // Start is called before the first frame update
     void Start()
     {
         createDict();
         publicCards = GameObject.Find("PublicCards").GetComponent<PublicCardSpawner>().allCards;
+
+        onNewPubilcCard += FilterFlopped;
 
         PublicCardSpawner.FlopAction += onFlop;
         PublicCardSpawner.TurnAction += onTurn;
@@ -24,41 +28,72 @@ public class PokerGameLogic : MonoBehaviour
 
     private void onFlop()
     {
-        filterFlopped();
+        FilterFlopped();
+        onNewPubilcCard?.Invoke();
         Debug.Log(playerCards.ToCommaSeparatedString());
     }
 
     private void onTurn()
     {
-        filterFlopped();
+        FilterFlopped();
+        onNewPubilcCard?.Invoke();
         Debug.Log(playerCards.ToCommaSeparatedString());
     }
 
     private void onRiver()
     {
-        filterFlopped();
+        FilterFlopped();
+        onNewPubilcCard?.Invoke();
         Debug.Log(playerCards.ToCommaSeparatedString());
+        winner = determineWinner(playerCards, publicCards);
     }
 
-    private void filterFlopped()
+    public GameObject getWinner()
     {
+        return winner;
+    }
+
+
+    private GameObject determineWinner(Dictionary<GameObject, Card[]> playerCards, Card[] publicCards)
+    {
+        GameObject bestPlayer = null;
+        GameObject bestHand = null;
+
+        
+        return new GameObject();
+    }
+
+
+    private void FilterFlopped()
+    {
+        List<GameObject> keysToRemove = new List<GameObject>();
+
         foreach (var cur in playerCards)
         {
             if (cur.Key.GetComponent<PlayerGameLogic>().isPlayerFlopped)
             {
-                playerCards.Remove(cur.Key);
+                keysToRemove.Add(cur.Key);
             }
+        }
+
+        foreach (var cur in keysToRemove)
+        {
+            playerCards.Remove(cur);
         }
     }
 
-   
 
     private void createDict()
     {
-        playerCards.Add(GameObject.Find("Player"), GameObject.Find("Player").GetComponent<PlayerGameLogic>().getCards());
-        playerCards.Add(GameObject.Find("Player (1)"), GameObject.Find("Player (1)").GetComponent<PlayerGameLogic>().getCards());
-        playerCards.Add(GameObject.Find("Player (2)"), GameObject.Find("Player (2)").GetComponent<PlayerGameLogic>().getCards());
-        playerCards.Add(GameObject.Find("Player (3)"), GameObject.Find("Player (3)").GetComponent<PlayerGameLogic>().getCards());
-        playerCards.Add(GameObject.Find("Player (4)"), GameObject.Find("Player (4)").GetComponent<PlayerGameLogic>().getCards());
+        playerCards.Add(GameObject.Find("Player"),
+            GameObject.Find("Player").GetComponent<PlayerGameLogic>().getCards());
+        playerCards.Add(GameObject.Find("Player (1)"),
+            GameObject.Find("Player (1)").GetComponent<PlayerGameLogic>().getCards());
+        playerCards.Add(GameObject.Find("Player (2)"),
+            GameObject.Find("Player (2)").GetComponent<PlayerGameLogic>().getCards());
+        playerCards.Add(GameObject.Find("Player (3)"),
+            GameObject.Find("Player (3)").GetComponent<PlayerGameLogic>().getCards());
+        playerCards.Add(GameObject.Find("Player (4)"),
+            GameObject.Find("Player (4)").GetComponent<PlayerGameLogic>().getCards());
     }
 }
