5a2ac09ca416967e5dae0f2e0db7010676149d22
gagewillette <gagew0513@gmail.com>
2023-09-24 21:27:51 -0400

    outline shader. fixed some file structuring

--- Assets/Scripts/PlayerLook.cs
+++ Assets/Scripts/PlayerLook.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerLook : MonoBehaviour
@@ -12,6 +13,8 @@ public class PlayerLook : MonoBehaviour
     private float xRotation;
     private float yRotation;
 
+    private int frameDelay = 0;
+    
     private void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
@@ -28,8 +31,32 @@ public class PlayerLook : MonoBehaviour
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90, 90);
         //yRotation = Mathf.Clamp(yRotation, -360, -90);
-        
+
         transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
         orientation.rotation = Quaternion.Euler(0, yRotation, 0);
+        
+        if (Physics.Raycast(transform.position, transform.forward, out RaycastHit hit, Mathf.Infinity))
+        {
+            frameDelay++;
+            if (frameDelay < 60)
+                return;
+            
+            if (!hit.transform.name.Contains("CasinoChip"))
+                return;    
+            if (Input.GetMouseButton(0))
+            {
+                if (!hit.transform.name.Contains("CasinoChip"))
+                    return;   
+            
+                Debug.Log("Clicking at chip");
+                return;
+            }
+
+            Outline obj = hit.transform.GetComponent<Outline>();
+            
+            obj.enabled = !obj.enabled;
+            
+            Debug.Log("Looking at chip");
+        }
     }
 }
